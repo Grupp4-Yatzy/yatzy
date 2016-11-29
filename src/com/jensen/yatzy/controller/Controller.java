404576785dd0be8450d2de8704117abe1e58ce2b
Benjamin Rosman <rosman.benji@gmail.com>
2016-11-29 10:31:01 +0100

    Created Window class

--- src/com/jensen/yatzy/controller/Controller.java
+++ src/com/jensen/yatzy/controller/Controller.java
@@ -2,4 +2,6 @@ package com.jensen.yatzy.controller;
 
 public class Controller {
 
+	
+	
 }
---
+++ src/com/jensen/yatzy/view/Window.java
@@ -0,0 +1,37 @@
+package com.jensen.yatzy.view;
+
+import java.awt.GraphicsConfiguration;
+import java.awt.HeadlessException;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class Window extends JFrame {
+
+	private JPanel currentPanel;
+	
+	public Window() {
+		super();
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		this.setTitle("Yatzy");
+	}
+	
+	public void changePanelTo(JPanel newPanel) {
+		if (this.currentPanel != null)
+			this.remove(this.currentPanel);
+		this.currentPanel = newPanel;
+		this.add(this.currentPanel);
+		this.repaint();
+		this.pack();
+	}
+	
+	public JPanel getCurrentPanel() {
+		return currentPanel;
+	}
+	
+	public void displayErrorMessage(String errorMessage) {
+		JOptionPane.showMessageDialog(this, errorMessage);
+	}
+
+}
