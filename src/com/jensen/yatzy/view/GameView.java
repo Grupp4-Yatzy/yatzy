cad41d79ac3fe8654dd723070398df0d993635f8
Robban82 <robin.java82@gmail.com>
2016-11-30 14:06:59 +0100

    added stuff

--- src/com/jensen/yatzy/view/GameView.java
+++ src/com/jensen/yatzy/view/GameView.java
@@ -1,4 +1,24 @@
 package com.jensen.yatzy.view;
-public class GameView{
 
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import javax.swing.*;
+
+public class GameView extends JPanel{
+    //JPanel eastPanel = new JPanel();
+    JPanel westPanel = new JPanel();
+    JPanel northPanel = new JPanel();
+    JPanel southPanel = new JPanel();
+    JPanel centerPanel = new JPanel();
+    JButton rollButton = new JButton();
+    JButton doneButton = new JButton();
+    JButton[] diceButtons = new JButton[5];
+    
+    public GameView (){
+        this.add(centerPanel, BorderLayout.CENTER);
+        this.add(westPanel, BorderLayout.WEST);
+        centerPanel.setBackground(Color.yellow);
+        westPanel.setBackground(Color.blue);
+    }
 }
