da61bf0feb15b5ba27c8ba8368d46986d30e803e
Robban82 <robin.java82@gmail.com>
2016-12-02 15:42:51 +0100

    test of gameview methods

--- src/com/jensen/yatzy/controller/Main.java
+++ src/com/jensen/yatzy/controller/Main.java
@@ -5,12 +5,22 @@ import com.jensen.yatzy.view.Window;
 
 public class Main {
 
+    public static final String[] combinations = {"Ettor", "Tvåor","Treor","Fyror","Femmor","Sexor",
+                            "Summa","Bonus","Ett par","Två par","Tretal","Fyrtal",
+                             "L.Stege","S.Stege","Kåk","Chans","¨Yatzy","Totalt"};
 	public static void main(String[] args) {
 		Window window = new Window();
                 GameView game = new GameView();
                 window.setCurrentPanel(game);
                 window.setVisible(true);
                 System.out.println(game.getHeight());
+                
+                String[] names = {"Benjamin", "Robin", "Roberto", "Kami"};
+                game.setPlayerNames(names);
+                game.setCombinations(combinations);
+                Integer[][] data = new Integer[combinations.length][names.length];
+                game.setTable(data, names);
+                window.update();
 	}
 
 }
