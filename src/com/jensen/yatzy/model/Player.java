0508c0d784d0033e3b9330ed945726643df44329
Benjamin Rosman <rosman.benji@gmail.com>
2016-12-07 08:39:06 +0100

    Created PLayer class with instance variables

---
+++ src/com/jensen/yatzy/model/Player.java
@@ -0,0 +1,15 @@
+package com.jensen.yatzy.model;
+
+public class Player {
+
+	private String name;
+	private Integer[] upperScore;
+	private int upperSum;
+	private boolean bonus;
+	private Integer[] lowerScore;
+	
+	public Player() {
+		// TODO Auto-generated constructor stub
+	}
+	
+}
