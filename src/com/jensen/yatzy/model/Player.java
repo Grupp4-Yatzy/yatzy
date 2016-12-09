f2f00f275bc965f7d7ebbca55abe162a24c51744
Robban82 <robin.java82@gmail.com>
2016-12-09 11:06:29 +0100

    implemented basic methods

--- src/com/jensen/yatzy/model/Player.java
+++ src/com/jensen/yatzy/model/Player.java
@@ -7,9 +7,29 @@ public class Player {
 	private int upperSum;
 	private boolean bonus;
 	private Integer[] lowerScore;
-	
+	private Integer[] scoreList;
+        
 	public Player() {
-		// TODO Auto-generated constructor stub
+            scoreList = new Integer[Constant.COMBINATIONS.length];
 	}
+        
+        public Integer[] getScore(){
+            return scoreList;
+        }
+        
+        public void addScore(int score, int index){
+            scoreList[index] = score;
+        }
+            
+        public String getName(){
+            return name;
+        } 
+        
+        public boolean isEmpty(int index){
+            if(scoreList[index]==null)
+                return true;
+            return false;
+        }
+            
 	
 }
