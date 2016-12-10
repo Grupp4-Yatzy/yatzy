3f29e4b901af9efd24011ee8eb65e6df8ad39d04
Benjamin Rosman <rosman.benji@gmail.com>
2016-12-10 17:19:22 +0100

    implemented addTotal()

--- src/com/jensen/yatzy/model/Player.java
+++ src/com/jensen/yatzy/model/Player.java
@@ -43,7 +43,11 @@ public class Player {
 	}
     
     public void addTotal() {
-		
+		int total = getScore(Constant.INDEX_OF_SUM) + getScore(Constant.INDEX_OF_BONUS);
+		for (int i = Constant.INDEX_OF_BONUS + 1; i < scoreList.length - 1; i++) {
+			total += getScore(i);
+		}
+		addScore(total, scoreList[scoreList.length - 1]);
 	}
     
     public int getFirstEmptyScoreIndex() {
