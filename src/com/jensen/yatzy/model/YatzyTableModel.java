fb4215e02c52ce110a6b177c48e2772884f2537d
Benjamin Rosman <rosman.benji@gmail.com>
2016-12-13 13:47:30 +0100

    Javdoc, and removed TODOs

--- src/com/jensen/yatzy/model/YatzyTableModel.java
+++ src/com/jensen/yatzy/model/YatzyTableModel.java
@@ -5,11 +5,22 @@ import java.awt.Component;
 import javax.swing.table.AbstractTableModel;
 import javax.swing.table.TableCellRenderer;
 
+/**
+ * Is a model of a yatzy score board
+ * @author benjamin
+ *
+ */
 public class YatzyTableModel extends AbstractTableModel {
     
     private Object[][] data;
     
-    public void initTable(int rowCount, int colCount){
+    /**
+     * Creates a new 2-dimensional Object array with the specified size from 
+     * the parameters.
+     * @param rowCount Number of rows
+     * @param colCount Number of columns
+     */
+    public void initTable(int rowCount, int colCount) {
         data = new Object[rowCount][colCount];
     }
     //Källa på följande kodstycke taget från https://www.youtube.com/watch?v=iMBfneE2Ztg
@@ -18,38 +29,25 @@ public class YatzyTableModel extends AbstractTableModel {
         return false;
     }
 
-    /*@Override
-    public Component prepareRenderer(TableCellRenderer renderer, int row, int column) {
-        Component c = super.prepareRenderer(renderer, row, column);
-        if (row % 2 == 0) {
-            c.setBackground(Color.LIGHT_GRAY);
-        } else {
-            c.setBackground(Color.WHITE);
-        }
-        return c;
-    }*/
-     //Slut på lånad kod
     @Override
     public int getRowCount() {
-        // TODO Auto-generated method stub
         return data.length;
     }
 
     @Override
     public int getColumnCount() {
-        // TODO Auto-generated method stub
         return data[0].length;
     }
 
     @Override
     public Object getValueAt(int rowIndex, int columnIndex) {
-        // TODO Auto-generated method stub
         return data[rowIndex][columnIndex];
     }
     
     @Override
-    public void setValueAt(Object value, int rowIndex, int columnIndex){
+    public void setValueAt(Object value, int rowIndex, int columnIndex) {
         data[rowIndex][columnIndex] = value;
+        fireTableCellUpdated(rowIndex, columnIndex);
     }
 
 }
