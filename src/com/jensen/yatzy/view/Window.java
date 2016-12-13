3cc81b59bc25e3d6e48137885d749a5245e1326c
Roberto Blanco <krull2001@hotmail.com>
2016-12-13 13:29:09 +0100

    JavaDoc implemented

--- src/com/jensen/yatzy/view/Window.java
+++ src/com/jensen/yatzy/view/Window.java
@@ -7,19 +7,32 @@ import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
+/**
+ * Window class is a JFrame that holds the currentPanel
+ * @author Benjamin Rosman
+ *  
+ */
 public class Window extends JFrame {
 
 	private JPanel currentPanel;
-	
+        
+	/**
+         * Constructor initialize super(), setTitle(), 
+         * setResizable() and setDefaultCloseOperation()
+         */
 	public Window() {
 		super();
 		this.setTitle("Yatzy");
-        this.setResizable(false);
+                this.setResizable(false);
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		
 	}
-	
-	
+        
+        /**
+         * This method removes the panel if its not empty
+         * and sets the currentPanel
+         * @param newPanel 
+         */
 	public void setCurrentPanel(JPanel newPanel) {
 		if (this.currentPanel != null)
 			this.remove(this.currentPanel);
@@ -28,16 +41,19 @@ public class Window extends JFrame {
 		this.repaint();
 		this.pack();
 	}
-        /*
-        public void update(){
-            this.repaint();
-            this.pack();
-        }*/
-	
+        
+        /**
+         * 
+         * @return the currentPanel
+         */
 	public JPanel getCurrentPanel() {
 		return currentPanel;
 	}
-	
+        
+	/**
+         * A window with an error message pop ups if the users input is incorrect
+         * @param errorMessage 
+         */
 	public void displayErrorMessage(String errorMessage) {
 		JOptionPane.showMessageDialog(this, errorMessage);
 	}
