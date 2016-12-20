ef42a1248b69d23e0a849fa6d2584db4ebb1ba9a
Benjamin Rosman <rosman.benji@gmail.com>
2016-12-20 22:27:30 +0100

    renamed getOkButton() to getPlayButton()

--- src/com/jensen/yatzy/controller/Controller.java
+++ src/com/jensen/yatzy/controller/Controller.java
@@ -116,7 +116,7 @@ public class Controller {
                     break;
             }
 
-            if (e.getSource() == newGamePanel.getOkButton()) {
+            if (e.getSource() == newGamePanel.getPlayButton()) {
                 initGame();
             } else if (e.getSource() == newGamePanel.getNumberOfPlayers()) {
                 createPlayerFields();
@@ -163,7 +163,7 @@ public class Controller {
             numberOfPlayers = Integer.parseInt(text);
             if (numberOfPlayers >= 1 && numberOfPlayers <= 6) {
                 newGamePanel.setPlayerFields(numberOfPlayers);
-                newGamePanel.getOkButton().setEnabled(true);
+                newGamePanel.getPlayButton().setEnabled(true);
             } else {
                 window.displayErrorMessage("Number of players must be [1-6].");
             }
@@ -185,7 +185,7 @@ public class Controller {
         newGamePanel.AddOptionListener(new OptionListener());
         newGamePanel.addMenuListener(new MenuListener());
         window.setCurrentPanel(newGamePanel);
-        newGamePanel.getOkButton().setEnabled(false);
+        newGamePanel.getPlayButton().setEnabled(false);
     }
 
     private void initGame() {
