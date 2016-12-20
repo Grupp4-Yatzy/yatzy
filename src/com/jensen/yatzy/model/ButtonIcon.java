74b6a8b5d949f06dfc13cabf7205496a8888a3a2
Benjamin Rosman <rosman.benji@gmail.com>
2016-12-21 00:36:16 +0100

    created ButtonIcon class

---
+++ src/com/jensen/yatzy/model/ButtonIcon.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.jensen.yatzy.model;
+
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+
+/**
+ *
+ * @author benjamin
+ */
+public class ButtonIcon {
+    
+        public static final Icon FRAME = new ImageIcon(Constant.class.getResource("/images/menubuttons/Frame.png"));
+        public static final Icon DISABLED_FRAME = new ImageIcon(Constant.class.getResource("/images/menubuttons/DisabledFrame.png"));
+        public static final Icon ROLL_OVER_FRAME = new ImageIcon(Constant.class.getResource("/images/menubuttons/RollOverFrame.png"));
+        public static final Icon PRESSED_FRAME = new ImageIcon(Constant.class.getResource("/images/menubuttons/PressedFrame.png"));
+        
+        public static final Icon SMALL_FRAME = new ImageIcon(Constant.class.getResource("/images/menubuttons/smallFrame.png"));
+        public static final Icon SMALL_DISABLED_FRAME = new ImageIcon(Constant.class.getResource("/images/menubuttons/smallDisabledFrame.png"));
+        public static final Icon SMALL_ROLL_OVER_FRAME = new ImageIcon(Constant.class.getResource("/images/menubuttons/smallRollOverFrame.png"));
+        public static final Icon SMALL_PRESSED_FRAME = new ImageIcon(Constant.class.getResource("/images/menubuttons/smallPressedFrame.png"));
+        
+    
+}
