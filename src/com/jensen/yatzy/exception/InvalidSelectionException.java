8b24a42627c2bc0fcabefdce1b9f8c0f7f3d5f3b
Benjamin Rosman <rosman.benji@gmail.com>
2017-01-05 12:56:23 +0100

    Removed empty constructor & updated javadoc for InvalidSelectionException

--- src/com/jensen/yatzy/exception/InvalidSelectionException.java
+++ src/com/jensen/yatzy/exception/InvalidSelectionException.java
@@ -1,18 +1,11 @@
 package com.jensen.yatzy.exception;
 
 /**
- * This class inherits from the super class Exception and is created to take care of user input that
- * is not valid.
+ * Exception that is thrown when a user has made an invalid selection.
  *
  * @author Benjamin Rosman, Roberto Blanco, Kami Hazzansadeh, Robin Nilsson
  */
 public class InvalidSelectionException extends Exception {
-
-  /**
-   * Creates a new instance of <code>InvalidSelection</code> without detail message.
-   */
-  public InvalidSelectionException() {
-  }
 
   /**
    * Constructs an instance of <code>InvalidSelection</code> with the specified detail message.
