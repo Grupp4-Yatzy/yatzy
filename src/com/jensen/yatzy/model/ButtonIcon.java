a8aa70fd9c285a1f3cc7db2f1e65cc5d6a01a9c9
Benjamin Rosman <rosman.benji@gmail.com>
2017-01-05 13:12:16 +0100

    added javadoc comments for ButtonIcon

--- src/com/jensen/yatzy/model/ButtonIcon.java
+++ src/com/jensen/yatzy/model/ButtonIcon.java
@@ -4,19 +4,28 @@ import javax.swing.Icon;
 import javax.swing.ImageIcon;
 
 /**
+ * Class for holding the constant icons used for the menu buttons.
  *
  * @author Benjamin Rosman
  */
 public class ButtonIcon {
 
+  /** Yellow frame icon for menu buttons */
   public static final Icon FRAME = new ImageIcon(Constant.class.getResource("/images/menubuttons/Frame.png"));
+  /** Disabled frame icon for menu buttons */
   public static final Icon DISABLED_FRAME = new ImageIcon(Constant.class.getResource("/images/menubuttons/DisabledFrame.png"));
+  /** Roll over frame icon for menu buttons */
   public static final Icon ROLL_OVER_FRAME = new ImageIcon(Constant.class.getResource("/images/menubuttons/RollOverFrame.png"));
+  /** Pressed frame icon for menu buttons */
   public static final Icon PRESSED_FRAME = new ImageIcon(Constant.class.getResource("/images/menubuttons/PressedFrame.png"));
 
+  /** Yellow frame icon for small menu buttons */
   public static final Icon SMALL_FRAME = new ImageIcon(Constant.class.getResource("/images/menubuttons/smallFrame.png"));
+  /** Disabled frame icon for small menu buttons */
   public static final Icon SMALL_DISABLED_FRAME = new ImageIcon(Constant.class.getResource("/images/menubuttons/smallDisabledFrame.png"));
+  /** Roll over frame icon for small menu buttons */
   public static final Icon SMALL_ROLL_OVER_FRAME = new ImageIcon(Constant.class.getResource("/images/menubuttons/smallRollOverFrame.png"));
+  /** Pressed frame icon for small menu buttons */
   public static final Icon SMALL_PRESSED_FRAME = new ImageIcon(Constant.class.getResource("/images/menubuttons/smallPressedFrame.png"));
 
 }
