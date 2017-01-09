f27fb62bdddd8e50a5f7829c5ec276a84492adbf
Benjamin Rosman <rosman.benji@gmail.com>
2017-01-09 17:25:37 +0100

    reorganized Constant class  & added number of rolls constant

--- src/com/jensen/yatzy/model/Constant.java
+++ src/com/jensen/yatzy/model/Constant.java
@@ -9,14 +9,14 @@ import java.awt.Color;
  */
 public class Constant {
 
-  /** Width of columns */
-  public static final int COLUMN_WIDTH = 100;
-
-  /** The score rewarded when a player gets yatzy */
-  public static final int YATZY = 50;
-
   /** The default number of dices */
   public static final int DEFUALT_NUMBER_OF_DICES = 5;
+  
+  /** Number of rolls */
+  public static final int NUMBER_OF_ROLLS = 3;
+
+  /** The defualt dice value */
+  public static final int DEFUALT_DICE_VALUE = 6;
 
   /** Score required for getting bonus in Forced mode */
   public static final int FORCED_REQUIRED_SCORE_FOR_BONUS = 42;
@@ -27,14 +27,8 @@ public class Constant {
   /** The Amount of bonus points recieved */
   public static final int BONUS = 50;
 
-  /** Index where the sum is shown */
-  public static final int INDEX_OF_SUM = 6;
-
-  /** Index where the bonus is shown */
-  public static final int INDEX_OF_BONUS = 7;
-
-  /** The defualt dice value */
-  public static final int DEFUALT_DICE_VALUE = 6;
+  /** The score rewarded when a player gets yatzy */
+  public static final int YATZY = 50;
 
   /** An array of all the combinations */
   public static final String[] COMBINATIONS = {"Ones", "Twos", "Threes",
@@ -42,8 +36,17 @@ public class Constant {
     "Sum", "Bonus", "One Pair", "Two Pair", "Three Of A Kind", "Four Of A Kind",
     "Small Straight", "Big Straight", "Fullhouse", "Chance", "Yatzy", "Total"};
 
+  /** Index where the sum is shown */
+  public static final int INDEX_OF_SUM = 6;
+
+  /** Index where the bonus is shown */
+  public static final int INDEX_OF_BONUS = 7;
+
   /** Index where the total is shown */
   public static final int INDEX_OF_TOTAL = COMBINATIONS.length - 1;
+
+  /** Width of columns */
+  public static final int COLUMN_WIDTH = 100;
 
   /** The background color (green) */
   public static final Color BG_COLOR_GREEN = Color.getHSBColor(0.3305556f, 1.0f, 0.74f);
